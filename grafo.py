e39c3b509491f733bbe1ec7c802512c83e2cca60
sorattorafa <38047989+sorattorafa@users.noreply.github.com>
2019-04-17 13:18:16 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -21,8 +21,10 @@ class Grafo:
 		self.vertices = {}
 		self.arestas = []
 		self.aresta_indices = {}  
-		self.time = 0
-
+		self.time = 0 
+		self.vetor_auxiliar = []
+		self.arestasaux = []  
+		self.componentes_separados = []
 	def add_vertice(self, vertice):
 		if isinstance(vertice, Vertice) and vertice.name not in self.vertices:
 			self.vertices[vertice.name] = vertice
@@ -81,18 +83,18 @@ class Grafo:
 			print(x)
 	def add_vetor_arestas(self,arestas): 
 		for aresta in arestas:
-			g.add_aresta(aresta[:1], aresta[2:])		
+			self.add_aresta(aresta[:1], aresta[2:])		
 	 
 	def add_vetor_vertices(self,str1,str2): 
 		for i in range(ord(str1), ord(str2)):
-			g.add_vertice(Vertice(chr(i)))  
+			self.add_vertice(Vertice(chr(i)))  
 			 
 	def get_adjacentes(self,vertice): 
 		if (vertice not in self.vertices): 
 			print('Vertice nao existe no grafo') 
 			return 
 		adjacentes = []   
-		for aresta in arestas:  
+		for aresta in self.arestasaux:  
 			if(aresta[:1] == vertice): 
 				adjacentes += aresta[2:] 
 		return adjacentes		  
@@ -102,7 +104,7 @@ class Grafo:
 			print('Vertice nao existe no grafo') 
 			return 
 		adjacentes = []   
-		for aresta in arestas:  
+		for aresta in self.arestasaux:  
 			if(aresta[2:] == vertice): 
 				adjacentes += aresta[:1] 
 		return adjacentes		  
@@ -112,7 +114,7 @@ class Grafo:
 			print('Vertice nao existe no grafo') 
 			return  
 		grau_saida = 0 
-		for aresta in arestas: 
+		for aresta in self.arestasaux: 
 			if(aresta[:1] == vertice): 
 				grau_saida +=1		   
 		return grau_saida  
@@ -122,7 +124,7 @@ class Grafo:
 			print('Vertice nao existe no grafo') 
 			return  
 		grau_entrada = 0 
-		for aresta in arestas: 
+		for aresta in self.arestasaux: 
 			if(aresta[2:] == vertice): 
 				grau_entrada +=1		  
 		return grau_entrada  
@@ -141,28 +143,19 @@ class Grafo:
 			print('Não é completo')				
 
 	def is_conexo(self):  
-		is_conexo = 0
-		auxizin = 0  
-		for v in self.vertices: 
-			if(self.get_grau_saida(v) == 0 and self.get_grau_entrada(v) == 0): 
-				auxizin = 1 
-			if(self.get_grau_saida(v) == 1 and self.get_grau_entrada(v) == 1): 
-				if(v in self.get_adjacentes(v)): 
-					auxizin = 1
-		 
-		if(auxizin == 1): 
-			is_conexo = 0 
-			return is_conexo
-		else:  
-			is_conexo = 1 
-			return is_conexo
-			 
+		# para ser conexo o grafo não pode ter 2 componentes separadas
+		# a busca em profundidade informa a quantidade de componentes 
+		# Logo : 
+		self.busca_em_profundidade()   
+		if(len(self.componentes_separados) > 1): 
+			return 0 
+		else: 
+			return 1	   				 
 	def is_arvore(self): 
-		if(self.is_conexo() == 1): 
-			if(len(self.vertices)-1 == len(arestas)): 
-				print('É arvore')   
-			else: 
-				print('Não é árvore')	
+		if(self.is_conexo() == 1 and len(self.vertices)-1 == len(self.arestasaux)):  
+			print('É arvore')   
+		else: 
+			print('Não é árvore')	
 				
 	def ftd_vertice(self,vertice): 
 		adja = []     
@@ -218,13 +211,13 @@ class Grafo:
 		vertice.mudar_cor('Cinza') 
 		advert = self.get_adjacentes(vertice.name) 
 		listaadjacentes = [] 
-		for auxiliar in vetor_auxiliar: 
+		for auxiliar in self.vetor_auxiliar: 
 			if(auxiliar.name in advert):  
 				listaadjacentes.append(auxiliar) 
 		for auxiliar in listaadjacentes: 
 			if(auxiliar.cor == 'Branco'): 
 				auxiliar.predecessor = vertice 
-				print(auxiliar.name) 
+#				print(auxiliar.name) 
 				self.dfs_visit(auxiliar)  
 		vertice.mudar_cor('Preto') 
 		self.time += 1
@@ -232,102 +225,37 @@ class Grafo:
 
 ## busca em profundidade 
 	def busca_em_profundidade(self):  
-		print('A ordem de nos é :')
-		for v in vetor_auxiliar:  
+		for v in self.vetor_auxiliar:  
 			v.mudar_cor('Branco') 
 		self.time = 0	  
-		for v in vetor_auxiliar: 
-			if(v.cor == 'Branco'): 
-				print(v.name)  
+		for v in self.vetor_auxiliar: 
+			if(v.cor == 'Branco'):  
+				if(v not in self.componentes_separados):
+					self.componentes_separados.append(v)
 				self.dfs_visit(v)
 ## busca em largura  
-	def busca_em_largura(self,vertice):  
-		print('A ordem percorrida é')
-		vertice.mudar_cor('Cinza') 
+	def busca_em_largura(self,vertice):   
+		# limpa as distâncias 
+		for d in self.vetor_auxiliar:   
+			d.d_inicial = 0 
+#		print('A ordem percorrida na busca em largura é')
+		vertice.mudar_cor('Cinza')  
+		vertice.d_inicial = 0
 		sequencia = []     
 		q = []
 		sequencia.append(vertice) 
-		q.append(vertice)
+		q.append(vertice) 
 		while len(q) > 0:
 			desempilhado = q[0]		 
 			q.pop(0)  
 			adj2 = self.get_adjacentes(desempilhado.name) 
-			for a in vetor_auxiliar: 
+			for a in self.vetor_auxiliar: 
 				if(a.name in adj2): 
 					if(a.cor == 'Branco'): 
 						a.mudar_cor('Cinza') 
+						a.d_inicial = desempilhado.d_inicial + 1  
 						q.append(a) 
 						sequencia.append(a)  
-			print(desempilhado.name)			
-			desempilhado.mudar_cor('Preto')			
-#		while len(sequencia) != len(vetor_auxiliar): 
-
-			
-# main  
-
-g = Grafo()
-# print(str(len(g.vertices)))
-u = Vertice('U')
-g.add_vertice(u) 
-v = Vertice('V')
-g.add_vertice(v) 
-w = Vertice('W')
-g.add_vertice(w) 
-x = Vertice('X')
-g.add_vertice(x) 
-y = Vertice('Y')
-g.add_vertice(y) 
-z = Vertice('Z')
-g.add_vertice(z)   
-
-vetor_auxiliar = [] 
-vetor_auxiliar.append(u)
-vetor_auxiliar.append(v) 
-vetor_auxiliar.append(w) 
-vetor_auxiliar.append(x) 
-vetor_auxiliar.append(y) 
-vetor_auxiliar.append(z)
-#g.add_vertice(Vertice('B'))  
-#cria um vetor de vertices de A té D
-#g.add_vetor_vertices('A','C')  
-
-#cria o vetor de arestas
-# insere tais arestas no grafo 
-arestas = ['U-V','U-X', 'X-V', 'Y-X', 'W-Y', 'W-Z', 'Z-Z','V-Y']
-g.add_vetor_arestas(arestas)  
-#mostra o grafo
-g.print_grafo()   
-# procura o grau de algum vertice no grafo 
-#g.get_grau('C')    
-# verifica se dois vertices são adjacentes
-#g.sao_adjacentes('C','C') 
-# mostra uma lista de vertices adjacentes a este vertice
-#g.get_adjacentes('A')   
-# todos vertices se ligam com todos 
-#g.is_completo() 
-#conexo = g.is_conexo()   
-
-#if(conexo == 1): 
-#	print('É conexo') 
-#g.is_arvore()    
-
-# fecho transitivo direto de um vértice do grafo 
-## = conjunto de vertices que se chega a partir 
-## deste vertice, lembrando que o grafo é digrafo 
-#g.ftd_vertice('B') 
- 
-# fecho transitivo indireto de um vértice do grafo 
-## é o conjunto de vertices que chegam nesse grafo 
-## por algum caminho existente  
-#g.fti_vertice('B')
-#g.get_grau_entrada('A')
-#g.get_grau_saida('A')
-#g.get_grau_entrada('A')  
-
-#g.getOrdem() 
-#g.getVertices()   
-  
- 
-# busca em largura 
-g.busca_em_largura(u)		 
-g.busca_em_profundidade()
+			maiordistancia = desempilhado.d_inicial			
+			desempilhado.mudar_cor('Preto')	 		 
+		return maiordistancia	
