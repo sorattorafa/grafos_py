29df60a346009d19f1399d31b4d2d4ca1eed761e
sorattorafa <38047989+sorattorafa@users.noreply.github.com>
2019-05-20 20:28:07 -0300

    algoritmo de kahn
    algoritmo de kahn

--- exercicio3.py
+++ exercicio3.py
@@ -32,30 +32,7 @@ for v in vertices:
 g.vetor_auxiliar = vetor_auxiliar 
 g.arestasaux = arestas	  
 # adiciona as arestas de fato
-g.add_vetor_arestas(arestas)  
-
-# em desenvolvimento, obrigado pela compreensão #	
-## algoritmo de kosajaru
-#g.busca_em_profundidade() 
-#matriztransposta = np.transpose(g.arestas)  
-#g.arestas = matriztransposta   
-#g.busca_em_profundidade() 
-#print(g.pilha_scc)  
-
-#print(matriztransposta)
-#for v, i in sorted(g.aresta_indices.items()):
-#	for j in range(len(g.arestas)):  
-#		if(g.arestas[i][j] != ''):
-#			g.arestas[i][j] = matriztransposta[i][j] 			
-#g.print_grafo()  
-
-# # componentes fortemente conexos ##
-#g.busca_em_profundidade()
-#g.busca_em_profundidade()
-#if(len(g.componentes_separados) > 1): 
-#	print('Sistema em Falha pois existem dois componentes desconexos')  
-#if(len(g.componentes_separados) == 1): 
-#	print('Sistema em Conectado totalmente')  
+g.add_vetor_arestas(arestas)   
 
 #algoritmo de khan  
 g.algoritimo_de_kahn() 
